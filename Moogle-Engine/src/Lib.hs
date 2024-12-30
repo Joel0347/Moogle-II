d5df7c4b2b16b5b4ceba47397faf24103c9018e3
ClaudiaHdezPerez <claudia.hdez.perez.2004@gmail.com>
2024-12-29 23:38:59 -0500

    quitar comentario en codigo

--- Moogle-Engine/src/Lib.hs
+++ Moogle-Engine/src/Lib.hs
@@ -18,37 +18,6 @@ type Document = (String, String)
 type Term = String
 type DocumentCollection = [Document]
 
--- calculateTF :: Term -> String -> Double
--- calculateTF term doc = 
---     let wordCount = length $ filter (== term) $ words doc
---         totalWords = length $ words doc
---     in if totalWords == 0 
---        then 0 
---        else fromIntegral wordCount / fromIntegral totalWords
-
--- calculateIDF :: Term -> DocumentCollection -> Double
--- calculateIDF term docs = 
---     let totalDocs = fromIntegral $ length docs
---         docsWithTerm = fromIntegral $ length $ filter (containsTerm term) docs
---     in if docsWithTerm == 0 
---        then 0 
---        else log (totalDocs / docsWithTerm)
---   where
---     containsTerm t (_, content) = t `elem` words content
-
--- calculateTFIDF :: Term -> Document -> DocumentCollection -> Double
--- calculateTFIDF term (_, content) docs = 
---     let tf = calculateTF term content
---         idf = calculateIDF term docs
---     in tf * idf
-
--- searchDocuments :: String -> DocumentCollection -> [(String, Double)]
--- searchDocuments query docs = 
---     let queryTerms = Data.List.words $ normalizeText query
---         scores = [(docName, sum [calculateTFIDF term (docName, content) docs | term <- queryTerms])
---                 | (docName, content) <- docs]
---     in reverse $ sortOn snd $ filter ((> 0) . snd) scores
-
 normalizeText :: String -> String
 normalizeText = map toLower . filter (not . isPunctuation)
 
