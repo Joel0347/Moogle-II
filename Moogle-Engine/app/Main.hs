f974972ac4f54a5dd32ab9cea4d500334b602b8f
ClaudiaHdezPerez <claudia.hdez.perez.2004@gmail.com>
2025-01-28 17:50:52 -0500

    agregar sugerencia

--- Moogle-Engine/app/Main.hs
+++ Moogle-Engine/app/Main.hs
@@ -10,53 +10,121 @@ import Network.Wai.Middleware.Static
 import Control.Monad.IO.Class
 import Network.HTTP.Types.Status
 import Web.Browser
-import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text as T
 import Data.Aeson (object, (.=))
-import Data.List (sortBy)
+import Data.List (sortBy, words)
 import Data.Ord (comparing)
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Control.Parallel.Strategies (parMap, rpar)
+import System.IO
 
 uploadFolder :: FilePath
 uploadFolder = "data"
 
+cacheFile :: FilePath
+cacheFile = "tfidf_cache.txt"
+
 staticFolder :: FilePath
 staticFolder = "static"
+
+-- Precomputar y guardar TF-IDF en un archivo de texto
+saveTfIdfCache :: FilePath -> Map String (Map String Double) -> IO ()
+saveTfIdfCache filename tfIdfDict = do
+    let serialized = show tfIdfDict
+    writeFile filename serialized
+
+-- Cargar TF-IDF desde un archivo de texto
+loadTfIdfCache :: FilePath -> IO (Map String (Map String Double))
+loadTfIdfCache filename = do
+    exists <- doesFileExist filename
+    if exists
+        then do
+            content <- readFile filename
+            return (read content)
+        else return Map.empty
+
+-- Crear un diccionario de frecuencias de palabras para la consulta
+queryWordFreq :: String -> Map String Int
+queryWordFreq query =
+    Map.fromListWith (+) [(word, 1) | word <- words $ normalize query]
+
+-- Buscar palabras de la consulta en el diccionario precomputado y sumar los valores de TF-IDF ponderados por su frecuencia en la consulta
+queryTfIdfSum :: Map String (Map String Double) -> Map String Int -> Map String Double
+queryTfIdfSum tfIdfDict queryFreq =
+    let wordTfIdf word = Map.findWithDefault Map.empty word tfIdfDict
+        docTfIdfSums = Map.foldrWithKey (\word freq acc -> Map.unionWith (+) (Map.map (* fromIntegral freq) (wordTfIdf word)) acc) Map.empty queryFreq
+    in docTfIdfSums
 
 main :: IO ()
 main = do
     -- Cargar documentos al inicio
     docs <- loadDocuments uploadFolder
 
+    -- Cargar TF-IDF desde la caché o recalcular si es necesario
+    tfIdfDictCache <- loadTfIdfCache cacheFile
+    tfIdfDict <- if Map.null tfIdfDictCache
+                 then do
+                     let newTfIdfDict = precomputeTfIdf docs
+                     saveTfIdfCache cacheFile newTfIdfDict
+                     putStrLn "TF-IDF cache generated and saved."
+                     return newTfIdfDict
+                 else do
+                     putStrLn "TF-IDF cache loaded."
+                     return tfIdfDictCache
+
+    let dictionary = buildDictionary(docs)
+    
     createDirectoryIfMissing True uploadFolder
     openBrowser "http://localhost:3000"
     
     scotty 3000 $ do
+        -- Middleware para servir archivos estáticos
         middleware $ staticPolicy (addBase staticFolder)
 
+        -- Ruta principal
         get "/" $ file $ staticFolder </> "index.html"
 
-        -- Nueva ruta para búsqueda
+        -- Ruta para búsqueda
         get "/search" $ do
             query <- param "query"
-            liftIO $ putStrLn $ "Query: " ++ T.unpack query
-            let queryText = ("query", T.unpack query) :: Document
-            let results = map (\doc -> (doc, jaccardSimilarity queryText doc)) docs
-            let filteredResults = filter (\(_, sim) -> sim > 0.1) results
+            let strictQuery = TL.toStrict query
+            let queryFreq = queryWordFreq (T.unpack strictQuery)
+            let tfIdfSums = queryTfIdfSum tfIdfDict queryFreq
+            let results = Map.toList tfIdfSums
+            let filteredResults = filter (\(_, sim) -> sim /= 0) results
             let sortedResults = sortBy (comparing (negate . snd)) filteredResults
-            json $ map (\(doc, sim) -> object ["document" .= fst doc, "similarity" .= sim]) sortedResults
+            if null sortedResults
+                then do
+                    let suggestions = take 1 $ sortBy (comparing snd) (suggestCorrection dictionary (TL.toStrict query))
+                    json $ map (\(suggestion, dist) -> object ["suggestion" .= suggestion, "distance" .= dist]) suggestions
+                else do
+                    json $ map (\(doc, sim) -> object ["document" .= doc, "similarity" .= sim]) sortedResults
 
-        -- Rutas existentes para archivos estáticos
+        -- Ruta para listar archivos
         get "/files" $ do
             files <- liftIO $ listDirectory uploadFolder
             json files
 
+        -- Ruta para descargar archivos
         get "/download/:filename" $ do
             filename <- param "filename"
-            let filePath = uploadFolder </> T.unpack filename
+            let filePath = uploadFolder </> TL.unpack filename
+            liftIO $ putStrLn $ "Attempting to download file: " ++ filePath
             fileExists <- liftIO $ doesFileExist filePath
+            liftIO $ putStrLn $ "File exists: " ++ show fileExists
             if fileExists
                 then do
-                    setHeader "Content-Disposition" ("attachment; filename=" `T.append` filename)
+                    liftIO $ putStrLn "Setting headers for file download..."
+                    setHeader "Content-Disposition" ("attachment; filename=" `TL.append` filename)
+                    setHeader "Content-Type" "application/octet-stream"
                     file filePath
-                else status status404 >> text "Archivo no encontrado."
+                else do
+                    liftIO $ putStrLn "File not found."
+                    status status404
+                    text "Archivo no encontrado."
 
+        -- Middleware para archivos en el directorio de subida
         middleware $ staticPolicy (addBase uploadFolder)
--- Moogle-Engine/src/Lib.hs
+++ Moogle-Engine/src/Lib.hs
@@ -1,9 +1,15 @@
 module Lib
     (
       loadDocuments
-    , jaccardSimilarity
+    , cosineSimilarity
     , Document
     , DocumentCollection
+    , buildDictionary
+    , suggestCorrection
+    , levenshtein
+    , tfIdf
+    , precomputeTfIdf
+    , normalize
     ) where
 
 import Data.List as DL
@@ -11,29 +17,83 @@ import Data.Char
 import qualified Data.Map as Map
 import System.Directory
 import Control.Monad
-import Data.Text (Text, words, unpack)
+import Data.Text (Text, words, unpack, pack)
 import qualified Data.Set as Set
+import Data.Array
+import Data.Set (Set)
+import Data.List (nub, sort, group)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Control.Parallel.Strategies (parMap, rpar)
 
 type Document = (String, String)
-type Term = String
 type DocumentCollection = [Document]
 
-normalizeText :: String -> String
-normalizeText = map toLower . filter (not . isPunctuation)
+-- Normaliza el texto eliminando puntuación y convirtiendo a minúsculas
+normalize :: String -> String
+normalize = map toLower . filter (`notElem` ",.!?\"'")
+
+-- Calcula la frecuencia de términos (TF) en un documento
+termFrequency :: String -> Map String Int
+termFrequency docContent = Map.fromListWith (+) [(word, 1) | word <- DL.words (normalize docContent)]
+
+-- Calcula las puntuaciones TF-IDF para un documento
+tfIdf :: DocumentCollection -> Map String Int -> Int -> Document -> Map String Double
+tfIdf corpus wordCounts numDocs doc =
+    let tf = termFrequency (snd doc)
+        totalWordsInDoc = sum (Map.elems tf)
+        idf = Map.mapWithKey (\word docFreq -> log (fromIntegral numDocs / fromIntegral (docFreq + 1))) wordCounts
+    in Map.mapWithKey (\word nd -> (fromIntegral nd / fromIntegral totalWordsInDoc) * Map.findWithDefault 0 word idf) tf
+
+-- Función para precomputar TF-IDF para todas las palabras en cada documento
+precomputeTfIdf :: DocumentCollection -> Map String (Map String Double)
+precomputeTfIdf docs =
+    let numDocs = length docs
+        wordCounts = foldr (\(_, content) acc -> foldr (\word -> Map.insertWith (+) word 1) acc (DL.words $ normalize content)) Map.empty docs
+        tfIdfMaps = parMap rpar (tfIdf docs wordCounts numDocs) docs
+        allWords = Set.toList $ buildDictionary docs
+        tfIdfForWord word = Map.fromList [(file, Map.findWithDefault 0 word tfIdfMap) | ((file, _), tfIdfMap) <- zip docs tfIdfMaps]
+    in Map.fromList [(word, tfIdfForWord word) | word <- allWords]
+
+-- Calcula la similitud entre dos documentos usando TF-IDF
+cosineSimilarity :: Map String Double -> Map String Double -> Double
+cosineSimilarity tfIdf1 tfIdf2 =
+    let dotProduct = sum [w1 * w2 | (term, w1) <- Map.toList tfIdf1, let w2 = Map.findWithDefault 0 term tfIdf2]
+        magnitude m = sqrt $ sum [w^2 | w <- Map.elems m]
+        mag1 = magnitude tfIdf1
+        mag2 = magnitude tfIdf2
+    in dotProduct / (mag1 * mag2)
 
 loadDocuments :: FilePath -> IO DocumentCollection
 loadDocuments dir = do
     files <- listDirectory dir
-    contents <- forM files $ \file -> do
+    forM files $ \file -> do
         content <- readFile (dir ++ "/" ++ file)
-        return (file, normalizeText content)
-    return contents
+        return (file, normalize content)
 
-jaccardSimilarity :: Document -> Document -> Double
-jaccardSimilarity (_, content1) (_, content2) =
-    let set1 = Set.fromList $ DL.words content1
-        set2 = Set.fromList $ DL.words content2
-        intersectionSize = Set.size $ Set.intersection set1 set2
-        unionSize = Set.size $ Set.union set1 set2
-    in fromIntegral intersectionSize / fromIntegral unionSize
+buildDictionary :: DocumentCollection -> Set.Set String
+buildDictionary documents = 
+    let allWords = concatMap (DL.words . snd) documents
+    in Set.fromList allWords
 
+-- Función para calcular la distancia de Levenshtein entre dos cadenas
+levenshtein :: String -> String -> Int
+levenshtein s1 s2 = levMemo (length s1) (length s2)
+  where
+    arr = array ((0, 0), (length s1, length s2))
+          [((i, j), lev i j) | i <- [0 .. length s1], j <- [0 .. length s2]]
+
+    lev 0 j = j
+    lev i 0 = i
+    lev i j = minimum [ arr ! (i-1, j) + 1
+                      , arr ! (i, j-1) + 1
+                      , arr ! (i-1, j-1) + (if s1 !! (i-1) == s2 !! (j-1) then 0 else 1)
+                      ]
+
+    levMemo i j = arr ! (i, j)
+
+-- Función ajustada para sugerir correcciones
+suggestCorrection :: Set String -> Text -> [(Text, Int)]
+suggestCorrection dictionary word =
+    let dictionaryList = map pack (Set.toList dictionary)
+    in map (\w -> (w, levenshtein (unpack w) (unpack word))) dictionaryList
